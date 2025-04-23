7bd0ad39122578fe92e5627c59cee6965646ddf1
wesly2000 <ylx.seu@foxmail.com>
2025-04-23 11:17:06 +0800

    test: real-world data line building

--- exp/tests/test_analyzer.py
+++ exp/tests/test_analyzer.py
@@ -514,8 +514,8 @@ def test_line_rel_building_01():
     tls_cell_4 = Cell(proto="tls", abs_frame_number=108)
     tls_cell_5 = Cell(proto="tls", abs_frame_number=109)
 
-    line = Line(upper_layer="http2", upper_cells=[http2_cell_1, http2_cell_2, http2_cell_3], 
-                lower_layer="tls", lower_cells=[tls_cell_1, tls_cell_2, tls_cell_3, tls_cell_4, tls_cell_5])
+    line = Line(upper_protocol="http2", upper_cells=[http2_cell_1, http2_cell_2, http2_cell_3], 
+                lower_protocol="tls", lower_cells=[tls_cell_1, tls_cell_2, tls_cell_3, tls_cell_4, tls_cell_5])
     
     line.lower_rel_building()
     line.upper_rel_building()
@@ -526,19 +526,29 @@ def test_line_rel_building_01():
     assert line.lower_rel_frame_number_map == target_lower_rel_frame_number_map 
 
     assert line.upper_abs_byte_map == target_upper_abs_byte_map
-
-# def test_line_rel_building_02():
-#     """
-#     This test covers building the lower relation of a line using real-world data.
-#     """
-#     tcp_filter = "tcp.stream == 0"
-#     keylog_file = "exp/test_dataset/realworld_dataset/decryption/keylog.txt"
-#     cap = pyshark.FileCapture(input_file=apple_file, display_filter=tcp_filter, 
-#                                 custom_parameters=["-C", "Customized", "-2"],
-#                                 override_prefs={'tls.keylog_file': os.path.abspath(keylog_file)})
     
-#     lower_rel_frame_number_map, upper_abs_byte_map = get_adjacent_protocol_reassemble_info(cap=cap, upper_layer="http2", lower_layer="tls")
+def test_line_rel_building_02():
+    """
+    This test covers building the lower relation of a line using real-world data.
+    """
+    tcp_filter = "tcp.stream == 2"
+    keylog_file = "exp/test_dataset/realworld_dataset/decryption/keylog.txt"
+    cap = pyshark.FileCapture(input_file=apple_file, display_filter=tcp_filter, 
+                                custom_parameters=["-C", "Customized", "-2"],
+                                override_prefs={'tls.keylog_file': os.path.abspath(keylog_file)})
+    
+    lower_rel_frame_number_map, upper_abs_byte_map = get_adjacent_protocol_reassemble_info(cap=cap, 
+                                                                                           upper_protocol="tls", 
+                                                                                           lower_protocol="tcp")
+    cap.close()
 
+    target_frame_number_map = {223: 0, 224: 1, 225: 2, 226: 3, 227: 4, 228: 5, 229: 6, 230: 7, 231: 8, 232: 9, 233: 10, 234: 11, 235: 12, 236: 13, 237: 14, 238: 15, 239: 16, 240: 17, 241: 18, 242: 19, 243: 20, 244: 21, 245: 22, 246: 23, 257: 24, 258: 25, 280: 26, 281: 27, 282: 28, 296: 29, 297: 30, 298: 31}
+
+    target_upper_abs_byte_map = {226: (0, 1908), 228: (1908, 3320), 230: (3320, 6004), 232: (6004, 6218), 234: (6218, 6298), 235: (6298, 6390), 236: (6390, 7436), 237: (7436, 7739), 238: (7739, 8042), 239: (8042, 8104), 241: (8104, 8135), 242: (8135, 8166), 244: (8166, 9096), 246: (9096, 9700), 257: (9700, 10305), 280: (10305, 10344), 281: (10344, 10368)}
+    
+
+    assert lower_rel_frame_number_map == target_frame_number_map
+    assert upper_abs_byte_map == target_upper_abs_byte_map
 
 def test_match_segment_number_01():
     """
