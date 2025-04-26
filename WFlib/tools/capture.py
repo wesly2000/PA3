8eeece22f6159c620fc92b69447d2b2749c26833
wesly2000 <ylx.seu@foxmail.com>
2025-04-26 09:54:56 +0800

    fix: waiting for Clash to launch

--- WFlib/tools/capture.py
+++ WFlib/tools/capture.py
@@ -273,7 +273,7 @@ def batch_capture(base_dir, host_list, iface,
         stdout = open(proxy_log, 'a+') if proxy_log is not None else subprocess.DEVNULL
 
         clash_process = subprocess.Popen(
-            ['/home/lxyu/clash/bin/clash-linux-amd64', '-key-trojan', keylog],
+            ['/home/lxyu/clash/bin/clash-linux-amd64-debug', '-key-vmess', keylog],
             stdout=stdout,
             stderr=subprocess.STDOUT 
         )
@@ -320,7 +320,7 @@ def batch_capture(base_dir, host_list, iface,
                 monitor_process = multiprocessing.Process(target=launch_proxy, kwargs={"keylog": keylog, "proxy_log": proxy_log})
                 monitor_process.start()
 
-                time.sleep(2)  # maybe waiting for proxy client to launch?
+                # time.sleep(2)  # maybe waiting for proxy client to launch?
             
             capture(url=url, 
                     timeout=timeout, 
