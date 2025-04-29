73ff90fc5964b1e2c72eddf428047fa9a617eb8e
wesly2000 <ylx.seu@foxmail.com>
2025-04-29 14:37:55 +0800

    feat: support caching arrays

--- exp/data_visualize/line_draw.py
+++ exp/data_visualize/line_draw.py
@@ -1,114 +1,61 @@
-import pyshark
-import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import os
 import argparse
-from tqdm import tqdm
+import seg_compute
 
-from WFlib.tools.visualize import *
-from WFlib.tools.capture import *
-from WFlib.tools.analyzer import *
+avg_color_map = {"normal": "blue", "vmess": "green"}
+std_color_map = {"normal": "yellow", "vmess": "purple"}
 
-def draw_byte_segment(avg_byte_segments: np.ndarray, std_byte_segments: np.ndarray, output_path: str):
+def draw_single_byte_segment(avg_byte_segments: np.ndarray, avg_color: str, std_byte_segments: np.ndarray, std_color: str):
+    """
+    Draw the byte segment array. The array is a list of segment index. The length of the list is the cutoff.
+    """
+    plt.plot(avg_byte_segments, '-', linewidth=1, color=avg_color)
+    plt.plot(std_byte_segments, '-', linewidth=1, color=std_color)
+
+
+def draw_byte_segment(root: str, host: str, SNI: str, output_path: str):
     """
     Draw the byte segment array. The array is a list of segment index. The length of the list is the cutoff.
     """
     plt.figure(figsize=(12, 6))
-    plt.plot(avg_byte_segments, '-', linewidth=1, color='blue', label='Average')
-    plt.plot(std_byte_segments, '-', linewidth=1, color='red', label='Standard Deviation')
+    for proto in avg_color_map:
+        avg_array_path = Path(os.path.join(root, "data_visualize", "result", f"avg_{host}_{SNI}_{proto}.npy"))
+        std_array_path = Path(os.path.join(root, "data_visualize", "result", f"std_{host}_{SNI}_{proto}.npy"))
+        if avg_array_path.exists() and std_array_path.exists():
+            print("Array exists, use stored array.")
+            avg_byte_segments, std_byte_segments = np.load(avg_array_path), np.load(std_array_path)
+        else:
+            print("Array does not exist, start computing...")
+            seg_compute.main(root, proto, host, SNI)
+
+            avg_byte_segments, std_byte_segments = np.load(avg_array_path), np.load(std_array_path)
+            draw_single_byte_segment(avg_byte_segments, avg_color_map[proto], std_byte_segments, std_color_map[proto])
     
     plt.xlabel('Byte Index')
     plt.ylabel('Stream Relative Segment Index')
     plt.title('Byte Segment Map')
+
     fig_format = output_path.split('.')[-1]
     plt.savefig(output_path, dpi=300, format=fig_format, bbox_inches='tight')
     plt.close()
 
-custom_parameters=["-C", "Customized", "-2"]
-
-def main():
-    def dry_run():
-        for file in sorted(pcap_dir.iterdir()):
-            if file.is_file() and file.suffix in ['.pcapng', '.pcap']:
-                try:
-                    tcp_stream, _ = h2data_SNI_intersect(file, SNIs, keylog_file=keylog_file, 
-                                                        custom_parameters=custom_parameters, 
-                                                        override_prefs=override_prefs)
-                except Exception as e:
-                    print(f"Error in file {file}: {e}")
-                    continue
-                tcp_stream_filter = stream_extract_filter(tcp_stream, [])
-                if tcp_stream_filter == "":
-                    continue
-
-                print(f"File {file.name} TCP filter: {tcp_stream_filter}")
-
-    def run():
-        if avg_array_path.exists() and std_array_path.exists():
-                print("The array has already been computed, use the stored value")
-                avg_byte_segments = np.load(avg_array_path)
-                std_byte_segments = np.load(std_array_path)
-        else:
-            lines = []
-            for file in tqdm(sorted(pcap_dir.iterdir())):
-                if file.is_file() and file.suffix in ['.pcapng', '.pcap']:
-                    try:
-                        tcp_stream, _ = h2data_SNI_intersect(file, SNIs, keylog_file=keylog_file, 
-                                                            custom_parameters=custom_parameters, 
-                                                            override_prefs=override_prefs)
-                    except Exception as e:
-                        print(f"Error in file {file}: {e}")
-                        continue
-                    tcp_stream_filter = stream_extract_filter(tcp_stream, [])
-                    if tcp_stream_filter == "":
-                        continue
-
-                    cap = pyshark.FileCapture(input_file=file, display_filter=tcp_stream_filter, 
-                                                custom_parameters=custom_parameters,
-                                                override_prefs=override_prefs)
-                    
-                    lines.append(get_adjacent_protocol_reassemble_info(cap, upper_protocol="http2", lower_protocol="tls"))
-
-                    cap.close()
-
-            byte_segments = generate_byte_segment(lines)
-            avg_byte_segments = np.mean(np.array(byte_segments), axis=0)
-            std_byte_segments = np.std(np.array(byte_segments), axis=0)
-
-            np.save(avg_array_path, avg_byte_segments)
-            np.save(std_array_path, std_byte_segments)
-
-        draw_byte_segment(avg_byte_segments, std_byte_segments, f"exp/data_visualize/img/{args.host}_{args.sni}_{args.protocol}.pdf")
-
-    
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Flag argument
-    parser.add_argument("-p", "--protocol", default="normal", type=str, help="The protocol to analyze")
     parser.add_argument("--host", required=True, type=str, help="The host to analyze")
     parser.add_argument("-s", "--sni", required=True, type=str, help="The SNI to analyze")
     parser.add_argument("-r", "--root", default="exp", type=str, help="The root directory of the capture and keylog")
-    parser.add_argument("--dry-run", action='store_true', help="Test the stream filter or other results instead of generating Lines")
+    # parser.add_argument("--dry-run", action='store_true', help="Test the stream filter or other results instead of generating Lines")
+    parser.add_argument("-o", "--output_path", type=str, help="The output path of the figure")
     args = parser.parse_args()
 
-    avg_array_path = Path(f"exp/data_visualize/result/avg_{args.host}_{args.sni}_{args.protocol}.npy")
-    std_array_path = Path(f"exp/data_visualize/result/std_{args.host}_{args.sni}_{args.protocol}.npy")
-
-    pcap_dir = Path(f"{args.root}/{args.protocol}_capture/{args.host}")
-    keylog_file = f"{args.root}/{args.protocol}_capture/{args.host}/keylog.txt"
-    proxy_keylog_file = f"{args.root}/{args.protocol}_capture/{args.host}/proxy_keylog.txt"
-
-    if args.protocol == 'normal':
-        override_prefs={'tls.keylog_file': os.path.abspath(keylog_file)}
-    elif args.protocol == 'vmess':
-        override_prefs={'tls.keylog_file': os.path.abspath(keylog_file),
-                        'vmess.keylog_file': os.path.abspath(proxy_keylog_file)}
-    SNIs = [args.sni]
-
-    if args.dry_run:
-        dry_run()
+    if args.output_path is None:
+        output_path = f"{args.root}/data_visualize/img/{args.host}_{args.sni}.pdf"
     else:
-        run()
+        output_path = args.output_path
 
-if __name__ == '__main__':    
-    main()
+    draw_byte_segment(args.root, args.host, args.sni, output_path)
---
+++ exp/data_visualize/seg_compute.py
@@ -0,0 +1,87 @@
+import pyshark
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import argparse
+from tqdm import tqdm
+
+from WFlib.tools.visualize import *
+from WFlib.tools.capture import *
+from WFlib.tools.analyzer import *
+
+custom_parameters=["-C", "Customized", "-2"]
+
+def extract_tcp_stream(pcap_file: Path, sni, keylog_file, custom_parameters, override_prefs):
+    """
+    Extract the proper TCP stream from the pcap file given the SNIs. If any error occurs, return an empty string.
+    """
+    try:
+        tcp_stream, _ = h2data_SNI_intersect(pcap_file, [sni], keylog_file=keylog_file, 
+                                            custom_parameters=custom_parameters, 
+                                            override_prefs=override_prefs)
+    except Exception as e:
+        print(f"Error in file {pcap_file}: {e}")
+        return ""
+    tcp_stream_filter = stream_extract_filter(tcp_stream, [])
+    if tcp_stream_filter == "":
+        print(f"Error in file {pcap_file}: No TCP stream found")
+        return ""
+    
+    return tcp_stream_filter
+
+def main(root, protocol, host, sni, dry_run=False):
+    pcap_dir = f"{root}/{protocol}_capture/{host}"
+    keylog_file = f"{pcap_dir}/keylog.txt"
+    proxy_keylog_file = f"{pcap_dir}/proxy_keylog.txt"
+
+    pcap_dir_path = Path(pcap_dir)
+
+    if protocol == 'normal':
+        override_prefs={'tls.keylog_file': os.path.abspath(keylog_file)}
+    elif protocol == 'vmess':
+        override_prefs={'tls.keylog_file': os.path.abspath(keylog_file),
+                        'vmess.keylog_file': os.path.abspath(proxy_keylog_file)}
+
+
+    lines = []
+    for file in tqdm(sorted(pcap_dir_path.iterdir())):
+        if file.is_file() and file.suffix in ['.pcapng', '.pcap']:
+            tcp_stream_filter = extract_tcp_stream(file, sni, keylog_file, custom_parameters, override_prefs)
+            if tcp_stream_filter == "":
+                continue
+
+            if dry_run:
+                print(f"File {file.name} TCP filter: {tcp_stream_filter}")
+                continue  # No actual computing in dry run mode
+
+            cap = pyshark.FileCapture(input_file=file, display_filter=tcp_stream_filter, 
+                                        custom_parameters=custom_parameters,
+                                        override_prefs=override_prefs)
+            
+            lines.append(get_adjacent_protocol_reassemble_info(cap, upper_protocol="http2", lower_protocol="tls"))
+            cap.close()
+
+    if not dry_run:
+        byte_segments = generate_byte_segment(lines)
+        avg_byte_segments = np.mean(np.array(byte_segments), axis=0)
+        std_byte_segments = np.std(np.array(byte_segments), axis=0)
+
+        avg_array_path = Path(f"exp/data_visualize/result/avg_{host}_{sni}_{protocol}.npy")
+        std_array_path = Path(f"exp/data_visualize/result/std_{host}_{sni}_{protocol}.npy")
+
+        np.save(avg_array_path, avg_byte_segments)
+        np.save(std_array_path, std_byte_segments)
+
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    # Flag argument
+    parser.add_argument("-p", "--protocol", default="normal", type=str, help="The protocol to analyze")
+    parser.add_argument("--host", required=True, type=str, help="The host to analyze")
+    parser.add_argument("-s", "--sni", required=True, type=str, help="The SNI to analyze")
+    parser.add_argument("-r", "--root", default="exp", type=str, help="The root directory of the capture and keylog")
+    parser.add_argument("--dry-run", action='store_true', help="Test the stream filter or other results instead of generating Lines")
+    args = parser.parse_args()
+
+    main(args.root, args.protocol, args.host, args.sni, args.dry_run)
