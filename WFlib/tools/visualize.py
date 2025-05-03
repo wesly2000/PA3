fix: wrong IQR computation
np.percentile uses 'linear' method to select the
inter-percentile value, which does not correspond
to IQR requirements. Changing to 'midpoint' solves
this issue. However, different version of Numpy uses
different params for it, e.g., 'interpolation' and
'method'.

To avoid version dependency, we manually computes
the IQR, and make it a util function for code reuse.

WFlib/tools/visualize.py
@@ -1,4 +1,5 @@
 from WFlib.tools.analyzer import Line 
+from WFlib.utils.statistics import IQR_bound
 from typing import List
 import numpy as np
 
@@ -28,14 +29,10 @@ def generate_byte_segment(lines: list[Line]) -> List[np.ndarray]:
     use IQR to filter out those lines that are too short.
     """
     byte_count_arr = np.array([line.byte_counter for line in lines])
-    Q1 = np.percentile(byte_count_arr, 25)  # Lower quartile (25th percentile)
-    Q3 = np.percentile(byte_count_arr, 75)  # Upper quartile (75th percentile)
+    lower_bound, upper_bound = IQR_bound(byte_count_arr)
 
-    IQR = Q3 - Q1  # Interquartile range (IQR)
-    lower_bound = Q1 - 1.5 * IQR  # Lower bound for outliers (adjust as needed)
-
-    # Filter out outliers (lines with byte_counter below the lower bound)
-    filtered_lines = [line for line in lines if line.byte_counter >= lower_bound]
+    # Filter out outliers (lines with byte_counter within the IQR range)
+    filtered_lines = [line for line in lines if lower_bound <= line.byte_counter <= upper_bound]
 
     # Calculate the minimum byte_counter among the filtered lines
     min_byte_count = min([line.byte_counter for line in filtered_lines])

exp/tests/test_visualize.py
@@ -24,23 +24,25 @@ def test_generate_byte_segment_01():
 
     http2_cell_4 = Cell(proto="http2", abs_frame_number=1107)
     http2_cell_4.abs_segment_frame_number = [1104, 1106, 1107]
-    http2_cell_4.segment_size = [2, 4, 3]
+    http2_cell_4.segment_size = [2, 4, 1]
     http2_cell_5 = Cell(proto="http2", abs_frame_number=1107)
     http2_cell_5.abs_segment_frame_number = [1107, 1107, 1107]
     http2_cell_5.segment_size = [1, 1, 2]
     http2_cell_6 = Cell(proto="http2", abs_frame_number=1109)
     http2_cell_6.abs_segment_frame_number = [1107, 1108, 1109]
-    http2_cell_6.segment_size = [2, 1, 2]
+    http2_cell_6.segment_size = [2, 1, 1]
 
     http2_line_long = Line(upper_protocol="http2", upper_cells=[http2_cell_4, http2_cell_5, http2_cell_6],
                            lower_protocol='tls', lower_abs_frame_numbers=[1104, 1106, 1107, 1108, 1109])
-
-    lines = [http2_line_short, http2_line_mid, http2_line_mid, http2_line_mid, http2_line_long]
+    # Make more samples to avoid IQR losing efforts on small datasets
+    lines = [http2_line_short, http2_line_mid, http2_line_mid, http2_line_mid, http2_line_mid, http2_line_mid, http2_line_long]
 
     # The shortest line should be removed, and the cutoff should be 13.
     byte_segments = generate_byte_segment(lines)
-    assert len(byte_segments) == 4, "The number of byte segments should be 4."
-    expected = [np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]), 
+    assert len(byte_segments) == 5, "The number of byte segments should be 4."
+    expected = [np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]),
+                np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]), 
+                np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]),
                 np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]),
                 np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3]),
                 np.array([0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])]