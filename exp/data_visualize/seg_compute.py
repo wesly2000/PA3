2c929af7219de430fa94b6f3ba950f03cd16c035
wesly2000 <ylx.seu@foxmail.com>
2025-05-11 12:11:54 +0800

    feat: separate input/output root

--- exp/data_visualize/seg_compute.py
+++ exp/data_visualize/seg_compute.py
@@ -31,8 +31,8 @@ def extract_tcp_stream(pcap_file: Path, sni, keylog_file, custom_parameters, ove
     
     return tcp_stream_filter
 
-def main(root, protocol, host, sni, dry_run=False):
-    pcap_dir = f"{root}/{protocol}_capture/{host}"
+def main(input_root, protocol, host, sni, base, output_root, dry_run=False):
+    pcap_dir = f"{input_root}/{protocol}_capture/{host}"
     keylog_file = f"{pcap_dir}/keylog.txt"
     proxy_keylog_file = f"{pcap_dir}/proxy_keylog.txt"
 
@@ -72,21 +72,39 @@ def main(root, protocol, host, sni, dry_run=False):
         avg_byte_segments = np.mean(np.array(byte_segments), axis=0)
         std_byte_segments = np.std(np.array(byte_segments), axis=0)
 
-        avg_array_path = Path(f"exp/data_visualize/result/avg_{host}_{sni}_{protocol}.npy")
-        std_array_path = Path(f"exp/data_visualize/result/std_{host}_{sni}_{protocol}.npy")
+        avg_array_path = Path(f"{output_root}/seg_compute/{base}/{protocol}/avg_{host}_{sni}.npy")
+        std_array_path = Path(f"{output_root}/seg_compute/{base}/{protocol}/std_{host}_{sni}.npy")
+
+        Path(avg_array_path).parent.mkdir(parents=True, exist_ok=True)
+        Path(std_array_path).parent.mkdir(parents=True, exist_ok=True)
 
         np.save(avg_array_path, avg_byte_segments)
         np.save(std_array_path, std_byte_segments)
 
 
 if __name__ == '__main__':    
+    """
+    Compute the byte segment of the given protocol, host, and SNI. Users should strictly follow the directory structure for convenient file management. See INPUT_CAPTURE in README.md for more details.
+
+    The output root is the root directory of the output, since VisualSeg would output many kind of results, e.g., the byte segment array, byte segment diagram, some statistics, etc., each method would have its own output directory under the SAME root.
+
+    For example, for seg_compute.py with protocol="normal", base="tls", the output directory is:
+    output_root/
+        seg_compute/
+            tls/
+                normal/
+                    avg_host_sni.npy
+                    std_host_sni.npy
+    """
     parser = argparse.ArgumentParser()
     # Flag argument
     parser.add_argument("-p", "--protocol", default="normal", type=str, help="The protocol to analyze")
     parser.add_argument("--host", required=True, type=str, help="The host to analyze")
     parser.add_argument("-s", "--sni", required=True, type=str, help="The SNI to analyze")
-    parser.add_argument("-r", "--root", default="exp", type=str, help="The root directory of the capture and keylog")
+    parser.add_argument("-b", "--base", default="tls", type=str, help="The lowest layer protocol as the segment index")
+    parser.add_argument("-i", "--input_root", required=True, type=str, help="The root directory of the capture and keylog")
+    parser.add_argument("-o", "--output_root", required=True, type=str, help="The root directory of the output")
     parser.add_argument("--dry-run", action='store_true', help="Test the stream filter or other results instead of generating Lines")
     args = parser.parse_args()
 
-    main(args.root, args.protocol, args.host, args.sni, args.dry_run)
+    main(args.input_root, args.protocol, args.host, args.sni, args.base, args.output_root, args.dry_run)
