7995450a6950c7b39d23ba4f9402d4febfb10abb
wesly2000 <ylx.seu@foxmail.com>
2025-05-12 16:49:55 +0800

    feat: add protocol check

--- WFlib/utils/config.py
+++ WFlib/utils/config.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 from configparser import ConfigParser
 
+SUPPORTED_BASE = ['tcp', 'tls']
+SUPPORTED_PROTOCOL = ['normal', 'vmess']
+
 def get_config(config_path: Path):
     if config_path.exists():
         config = ConfigParser()
