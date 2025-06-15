5582c21b71a8f341ddfafa272e39bb70e7d6afab
wesly2000 <ylx.seu@foxmail.com>
2025-06-15 12:07:26 +0800

    test: NpzHSDBSExtractor

--- exp/tests/test_extractor.py
+++ exp/tests/test_extractor.py
@@ -3,6 +3,7 @@ import io
 import numpy as np
 import pandas as pd
 import pytest
+from tempfile import TemporaryFile
 
 from WFlib.tools.extractor import *
 from WFlib.tools.formatter import PcapFormatter
@@ -260,3 +261,53 @@ def test_pcap_to_dataframe_3():
     extractor = CsvLenExtractor()
     result = extractor.extract(df, protocol="udp")
     assert result[0] == 1260
+
+
+
+@pytest.fixture
+def npz_files():
+    # Create 3 temporary .npz files
+    stream_1 = {
+        "direction": np.array([1, -1, 1, -1, 1, -1, -1, 1, -1, 1]),
+        "length": np.array([100, 20, 100, 32, 100, 20, 100, 20, 100, 20]),
+        "timestamp": np.array([3, 4, 5, 6, 7, 8, 11, 12, 13, 14])
+    }
+
+    stream_2 = {
+        "direction": np.array([-1, 1, -1, 1, -1, 1, -1, 1]),
+        "length": np.array([100, 20, 100, 32, 100, 20, 100, 20]),
+        "timestamp": np.array([9, 10, 15, 16, 17, 18, 19, 20])
+    }
+
+    stream_3 = {
+        "direction": np.array([1, 1, -1, -1, 1, 1, -1, 1, -1]),
+        "length": np.array([100, 100, 31, 100, 20, 100, 20, 100, 20]),
+        "timestamp": np.array([0, 1, 2, 21, 22, 23, 24, 25, 26])
+    }
+
+    # Convert the streams to npz objects
+    tmp_file_1, tmp_file_2, tmp_file_3 = TemporaryFile(), TemporaryFile(), TemporaryFile()
+    np.savez(tmp_file_1, **stream_1)
+    np.savez(tmp_file_2, **stream_2)
+    np.savez(tmp_file_3, **stream_3)
+
+    tmp_file_1.seek(0)
+    tmp_file_2.seek(0)
+    tmp_file_3.seek(0)
+
+    npz_files = [np.load(tmp_file_1), np.load(tmp_file_2), np.load(tmp_file_3)]
+
+    return npz_files
+    
+
+def test_NpzHSDBSExtractor_1(npz_files):
+    """
+    Test reading a .npz file and extract the hsdbs feature.
+    """
+    extractor = NpzHSDBSExtractor()
+    result = []
+    extractor.extract(result, npz_files)
+
+    target = np.array([200, -100, 100, 0, 100, 0, 100, -100, -100, 0, 0, -100, 0, -100, 0, -100, 0, -100, 0, 100, 0, 100, 0])
+    assert np.all(result == target)
+
