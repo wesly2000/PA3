9203b893f25eaa9369b40768643809cd4412fa3b
wesly2000 <ylx.seu@foxmail.com>
2025-07-10 12:17:12 +0800

    test: update NpzExtractor tests

--- exp/tests/test_extractor.py
+++ exp/tests/test_extractor.py
@@ -331,7 +331,7 @@ def test_NpzHSDBSExtractor_4(npz_buffers):
     Test reading a .npz file and extract the hsdbs feature, this test covers the case that the criterion option is set to BSExcludeCriterion.
     """
     extractor = NpzHSDBSExtractor(
-        criteria=BSExcludeCriterion(lower_bounds=np.array([350]), upper_bounds=np.array([450]), threshold=0), 
+        criteria=HSDBSExcludeCriterion(lower_bounds=np.array([350]), upper_bounds=np.array([450]), threshold=32), 
         ignore_control_packets=True, threshold=32)
     result = []
     npz_files = [np.load(npz_buffer) for npz_buffer in npz_buffers]
@@ -347,7 +347,10 @@ def test_NpzHSDBSExtractor_5(npz_buffers):
     """
     Test reading a .npz file and extract the hsdbs feature, this test covers the case that the criterion option is set to BSExcludeCriterion.
     """
-    extractor = NpzHSDBSExtractor(criteria=BSExcludeCriterion(lower_bounds=np.array([350, 460]), upper_bounds=np.array([450, 550])), ignore_control_packets=True, threshold=32)
+    extractor = NpzHSDBSExtractor(
+        criteria=HSDBSExcludeCriterion(lower_bounds=np.array([350, 460]), upper_bounds=np.array([450, 550]), threshold=32), 
+        ignore_control_packets=True, 
+        threshold=32)
     result = []
     npz_files = [np.load(npz_buffer) for npz_buffer in npz_buffers]
     extractor.extract(result, npz_files)
@@ -397,7 +400,7 @@ def test_NpzDirExtractor_4(npz_buffers):
     """
     Test reading a .npz file and extract the hsdbs feature, this test covers the case that the criterion option is set to BSExcludeCriterion.
     """
-    extractor = NpzDirExtractor(stripper=VmessStripper(), criteria=BSExcludeCriterion(lower_bounds=np.array([350, 460]), upper_bounds=np.array([450, 550]), threshold=32))
+    extractor = NpzDirExtractor(stripper=VmessStripper(), criteria=HSDBSExcludeCriterion(lower_bounds=np.array([350, 460]), upper_bounds=np.array([450, 550]), threshold=32))
     result = []
     npz_files = [np.load(npz_buffer) for npz_buffer in npz_buffers]
     extractor.extract(result, npz_files)
