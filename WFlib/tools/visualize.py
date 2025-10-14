75288b8df8167de4644507d6b636af3e9f351869
wesly2000 <ylx.seu@foxmail.com>
2025-10-14 09:14:55 +0800

    feat: 2D stream visualization

--- WFlib/tools/visualize.py
+++ WFlib/tools/visualize.py
@@ -105,9 +105,9 @@ def stream_feature_2D(npz_files: List[NpzFile], extractor: NpzExtractor):
     Stream level feature extraction, one npz_file represents one stream, we require generally the meta information of the stream being (feature_ts, feature) list, and extract the timestamp and feature series for each stream.
     """
 
-    BIN_RANGE = 50000
-    BIN_STEP = 500
-    BINS = np.arange(-BIN_RANGE, BIN_RANGE + BIN_STEP, BIN_STEP)
+    # BIN_RANGE = 50000
+    # BIN_STEP = 500
+    # BINS = np.arange(-BIN_RANGE, BIN_RANGE + BIN_STEP, BIN_STEP)
 
     yz_2D = []
     stream_start_times = []
@@ -117,25 +117,67 @@ def stream_feature_2D(npz_files: List[NpzFile], extractor: NpzExtractor):
         feature = np.array([s[1] for s in stream])
         # Add bins to the feature and clip the feature to the range [lower_bound, upper_bound]
         # Create a mask for non-zero elements
-        non_zero_mask = feature != 0
+        # non_zero_mask = feature != 0
         # Initialize result array with zeros
-        binned_feature = np.zeros_like(feature)
+        # binned_feature = np.zeros_like(feature)
 
         # Only apply binning to non-zero elements
-        if np.any(non_zero_mask):
-            bin_idx = np.digitize(feature[non_zero_mask], BINS) - 1
-            bin_idx = np.clip(bin_idx, 0, len(BINS) - 2)
-            binned_feature[non_zero_mask] = (BINS[bin_idx] + BINS[bin_idx + 1]) / 2
+        # if np.any(non_zero_mask):
+        #     bin_idx = np.digitize(feature[non_zero_mask], BINS) - 1
+        #     bin_idx = np.clip(bin_idx, 0, len(BINS) - 2)
+        #     binned_feature[non_zero_mask] = (BINS[bin_idx] + BINS[bin_idx + 1]) / 2
 
-        yz_2D.append((timestamp, binned_feature))
+        yz_2D.append((timestamp, feature))
 
         stream_start_times.append(np.min(timestamp))
 
     start_time = np.min(stream_start_times)
-    yz_2D = [(timestamp - start_time, binned_feature) for timestamp, binned_feature in yz_2D]
+    # yz_2D = [(timestamp - start_time, binned_feature) for timestamp, binned_feature in yz_2D]
+    yz_2D = [(timestamp - start_time, feature) for timestamp, feature in yz_2D]
 
     return yz_2D
 
+
+def stream_feature_2D_draw(host: str, sni: str, xy: List[List[Tuple[ArrayLike, ArrayLike]]], bar_width: float=0.1, cmap_name: str='tab10', alpha: float=0.8):
+    """
+    Draw bin graph for feature 2D.
+    """
+    x_limit = 3
+    xy.sort(key=lambda x: len(x[0]), reverse=True)
+    cmap = ["#033BE4", "#069404", "#D27000", "#9C0000", "#C602E0", "#028067"]
+    fig, ax = plt.subplots(1, 1, figsize=(4, 1.5), constrained_layout=True)
+
+    ax.axhline(y=0, color='black', linestyle='--', lw=.5)
+    for c, (x, y) in enumerate(xy):
+        x_mask = x < x_limit
+        x = x[x_mask]
+        y = y[x_mask]
+
+        color = cmap[c]    
+        positive_y_mask = y > 0
+        positive_y = y[positive_y_mask]
+        x_for_positive_y = np.round(x[positive_y_mask], 2)
+        unique_x_for_positive_y, indices = np.unique(x_for_positive_y, return_inverse=True)
+        positive_sums = np.bincount(indices, weights=positive_y)
+        ax.bar(unique_x_for_positive_y, positive_sums, color=color, width=bar_width)
+        # ax.plot(unique_x_for_positive_y, positive_sums, color=color)
+
+        negative_y_mask = y < 0
+        negative_y = y[negative_y_mask]
+        x_for_negative_y = np.round(x[negative_y_mask], 2)
+        unique_x_for_negative_y, indices = np.unique(x_for_negative_y, return_inverse=True)
+        negative_sums = np.bincount(indices, weights=negative_y)
+        # ax.plot(unique_x_for_negative_y, negative_sums, color=color)
+        
+        # ax.bar(x, y, color=color, alpha=0.8, width=bar_width, linewidth=.5, edgecolor='black')
+        ax.bar(unique_x_for_negative_y, negative_sums, color=color, width=bar_width)
+        
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Size')
+    ax.set_title(f'{host}, {sni}')
+
+    # plt.tight_layout()
+    plt.show()
 
 def stream_feature_3D_draw(host: str, sni: str, feature: str, yz_3D: List[List[Tuple[ArrayLike, ArrayLike]]], bar_width: float=0.3, cmap_name: str='tab10', alpha: float=0.8):
     """
@@ -148,8 +190,28 @@ def stream_feature_3D_draw(host: str, sni: str, feature: str, yz_3D: List[List[T
     ax = fig.add_subplot(111, projection='3d')
     ax.set_box_aspect([1, 5, 1])
     for x, yz_2D in enumerate(yz_3D):
-        yz_2D.sort(key=lambda x: len(x[0]), reverse=True)
+        yz_2D.sort(key=lambda x: max(x[0]), reverse=True)
         for c, (y, z) in enumerate(yz_2D):
+            # y = np.array(y)
+            # z = np.array(z)
+            # mask = y <= 60
+            # y = y[mask]
+
+            # if len(y) == 0:
+            #     continue
+            # z = z[mask]
+
+            # # Create arrays for bar3d
+            # xs = np.full_like(y, x)  # Same x for the whole group
+            # ys = y
+            # zs = np.zeros_like(z)
+
+            # dx = np.ones_like(y) * bar_width
+            # dy = np.ones_like(y) * bar_width
+            # dz = z
+
+            # color = cmap(c)            
+            # ax.bar3d(xs, ys, zs, dx, dy, dz, color=color, alpha=0.8)
             y = np.array(y)
             y = np.clip(y, 0, 20)
             z = np.array(z)
