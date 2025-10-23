bc4c8d80ae2f827b3a90cf7cff73eb45301f4e45
wesly2000 <ylx.seu@foxmail.com>
2025-10-23 11:17:37 +0800

    feat: unified module for transformed features

---
+++ exp/dataset_process/gen_transformed.py
@@ -0,0 +1,56 @@
+# Generates the transformed traffic features. 
+import numpy as np
+import os
+import argparse
+from typing import List
+import time
+import random
+import torch
+from tqdm import tqdm
+from multiprocessing import Process
+from WFlib.tools import data_processor
+
+# Set a fixed seed for reproducibility
+fix_seed = 2024
+random.seed(fix_seed)
+torch.manual_seed(fix_seed)
+np.random.seed(fix_seed)
+
+parser = argparse.ArgumentParser(description='Feature extraction')
+# parser.add_argument("--dataset", type=str, required=True, default="Undefended", help="Dataset name")
+parser.add_argument("--seq_len", type=int, default=5000, help="Input sequence length")
+parser.add_argument("--input_file", "-i", type=str, help="input file")
+parser.add_argument("--output_file", "-o", type=str, required=True, help="output file")
+parser.add_argument("--feature", "-f", type=str, required=True, help="Feature used, option=[TAM, TSAM]")
+# Params specific to TAM/TASM
+parser.add_argument("-t", type=int, default=80, help="Maximum load time for packets")
+parser.add_argument("-l", type=int, default=1800, help="Maximum length of the matrix")
+args = parser.parse_args()
+
+if not os.path.exists(args.input_file):
+    raise FileNotFoundError(f"The dataset path does not exist: {args.input_file}")
+
+feature = args.feature.lower()
+
+if not os.path.exists(args.output_file):
+    data = np.load(args.input_file, allow_pickle=True)
+    X = data["raw"]
+    # Data alignment
+    if feature in ['tam', 'tsam']:
+        if X.shape[1] > args.seq_len:  # Truncate along axis 1
+            X = X[:,:args.seq_len,:]
+        else:
+            padding_num = args.seq_len - X.shape[1]
+            pad_width = [(0, 0), (0, padding_num), (0, 0)]
+            X = np.pad(X, pad_width=pad_width, mode="constant", constant_values=0)  # Pad the sequence with zeros
+    else:
+        X = data_processor.length_align(X, args.seq_len)
+
+    transformed = {'labels': data["labels"], 'hosts': data["hosts"]}
+    
+    if feature == 'tam':
+        transformed[feature] = data_processor.extract_TAM(X, args.t, args.l)
+    elif feature == 'tsam':
+        transformed[feature] = data_processor.extract_TSAM(X, args.t, args.l)
+    
+    np.savez_compressed(args.output_file, **transformed)
